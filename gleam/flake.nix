3d865b6aa75d40967b42d9f999e13b602f03ed01
Pedraza Vázquez, Francisco <fpedrazav02@gmail.com>
2025-09-25 19:51:27 +0200

    feat(Gleam): Add basic gleam shell

---
+++ gleam/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "Development Gleam flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            gleam
+            erlang
+            nodejs_22
+          ];
+        };
+      }
+    );
+
+}
