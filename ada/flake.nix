ee0cec04341d42484666df35a7f9d4bc956011ec
Francisco Pedraza Vázquez <fpedrazav02@gmail.com>
2025-10-15 16:58:13 +0200

    Update flake.nix

--- ada/flake.nix
+++ ada/flake.nix
@@ -12,7 +12,7 @@
 
     adaLanguageServer = pkgs.stdenv.mkDerivation rec {
       pname = "ada-language-server";
-      version = "2026.0";
+      version = "2026.0.202510141";
 
       src = pkgs.fetchurl {
         url = "https://github.com/AdaCore/ada_language_server/releases/download/${version}/als-${version}-linux-x64.tar.gz";
