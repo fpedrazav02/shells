2c13c516ae266cec11108b9756f96d8f254e99a1
Francisco Pedraza Vázquez <fpedrazav02@gmail.com>
2025-11-15 18:41:16 +0100

    Update flake.nix

--- kotlin/flake.nix
+++ kotlin/flake.nix
@@ -1,1 +1,35 @@
+{
+  description = "Kotlin development shell";
 
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            kotlin
+            ktlint
+            detekt
+            maven
+            gradle
+            kotlin-language-server
+            yaml-language-server
+            git
+          ];
+        };
+      }
+    );
+}
